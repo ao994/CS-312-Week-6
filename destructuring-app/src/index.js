cadb58157d519142e82af8bc59f672b3d6989e30
Alyssa Ortiz <ao994@nau.edu>
2024-07-17 22:08:43 -0700

    Added all files

---
+++ destructuring-app/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import cars from './practice';
+
+const [honda, tesla] = cars;
+
+const {speedStats: {topSpeed: hondaTopSpeed}} = honda;
+const {speedStats: {topSpeed: teslaTopSpeed}} = tesla;
+
+const {coloursByPopularity: [hondaTopColour]} = honda;
+const {coloursByPopularity: [teslaTopColour]} = tesla;
+
+//changed; ReactDOM.render is depreciated
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>
+);
+
